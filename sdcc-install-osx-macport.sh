6675c7d28b4361135452b0005134d601a2ebefa4
Ekawahyu Susilo <ekawahyu@yahoo.com>
2019-07-15 13:17:32 -0700

    added CPPFLAGS to include boost header files with macports

--- sdcc-install-osx-macport.sh
+++ sdcc-install-osx-macport.sh
@@ -79,7 +79,7 @@ patch -p 1 < $distribution_patch
 #
 echo "Installing SDCC for Mac OSX..."
 
-./configure \
+./configure CPPFLAGS=-I/opt/local/include \
 --prefix="/usr/local" \
 --disable-z80-port --disable-z180-port --disable-r2k-port --disable-r3ka-port \
 --disable-gbz80-port --disable-ds390-port --disable-ds400-port --disable-pic14-port \
